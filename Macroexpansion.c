1fe976b7c5fcbbad6009bbf8c15f3d68ed3b56fa
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-10-01 19:08:42 +0530

    Create Macroexpansion.c

---
+++ Macroexpansion.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void macro_replace(FILE *,FILE *);
+int main(int argc,char *argv[])
+{
+	int i;
+	FILE *fsrc,*fdest;
+	if(argc!=3)
+	{
+		printf("Improper number of arguments\n");
+		exit(0);
+	}
+	fsrc=fopen(argv[1],"r");
+	fdest=fopen(argv[2],"w");
+	macro_replace(fsrc,fdest);
+	printf("macro identifier is replaced with token sequence\n");
+	printf("Open the output file\n");
+}
+void macro_replace(FILE *input,FILE *output)
+{
+	int i=0,j=0,k=0;
+	char c,m[5],buf[7],t[4],temp[4],x[4];
+	int len,len1,len2;
+	char s[7]="#define";
+	while((c=fgetc(input))!=EOF)
+	{
+		if(c==s[0])
+		{
+			fputc(c,output);
+			buf[0]=c;
+			for(i=1;i<=6;i++)
+			{
+				c=fgetc(input);	
+				fputc(c,output);
+				if(c==s[i])
+				{
+					buf[i]=c;
+				}
+				else
+				fputc(c,output);
+			}
+			len=strlen(buf);
+			buf[len+1]='\0';
+			//printf("\n");
+			c=fgetc(input);	
+			if(c==' ')
+				fputc(c,output);
+			do
+			{
+				fputc(c,output);
+				m[j]=c;//to store char carried from while
+				j++;
+				c=fgetc(input);
+				m[j]=c;
+				j++;
+				fputc(c,output);
+				m[j]=c;
+			
+			}while((c=fgetc(input))!=' ');
+			m[j]='\0';
+			//printf(" m is %s\n ",m);
+			len1=strlen(m);
+			printf("\n");
+			do
+			{	fputc(c,output);
+				t[k]=c;//to store char carried from while
+				k++;
+				c=fgetc(input);
+				t[k]=c;//to store next char
+				k++;
+				fputc(c,output);
+				t[k]=c;
+			
+			}while((c=fgetc(input))!='\n');
+			fputc('\n',output);
+			t[k]='\0';
+			//printf(" t is %s\n ",t);
+			//printf("\n");
+			while(c!=EOF)
+			{
+				for(i=0;i<len1;i++)
+				{
+					c=fgetc(input);
+					if(c==m[i])
+					{
+						temp[i]=t[i];
+						
+						fputc(temp[i],output);		
+						//printf("%c",temp[i]);
+					}
+					else
+					fputc(c,output);
+				}
+			}
+			temp[i]='\0';
+		}//if
+		else
+		{
+			fputc(c,output);
+		}			
+	}
+}
+
