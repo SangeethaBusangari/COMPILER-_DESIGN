0fe8a28a0e1611c7237c437380018991b596e8f6
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-10-01 19:08:05 +0530

    Create inputforremovecomments.c

---
+++ inputforremovecomments.c
@@ -0,0 +1,16 @@
+//iam sangeetha
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+	//
+//dfdgh
+//...
+	int i,j;
+	printf("enter\**/ number");
+	printf("sangeetha is \\"good"\ girl");
+	
+}
+
+/*INSIDE THE PRINTF STATEMENT MUST PRINT 
+		THE COMMENT LINES*/
